14e1e104ab46aa9133d375d16bac72e90133f6e3
megaserg <hellomind@gmail.com>
2011-10-23 20:19:44 +0400

    Added create_empty.sql for creation of empty database.

---
+++ storage/create_empty.sql
@@ -0,0 +1,16 @@
+/*
+Created: 04.10.2011
+Modified: 23.10.2011
+Project: goodsreview_permanent
+Model: GoodsReviews
+Author: Sergey Serebryakov
+Database: MySQL 5.5
+*/
+
+-- Drop database section -----------------------------------------------
+
+DROP DATABASE IF EXISTS goodsreview_permanent;
+
+-- Create database section ---------------------------------------------
+
+CREATE DATABASE goodsreview_permanent CHARACTER SET utf8 COLLATE utf8_bin;
